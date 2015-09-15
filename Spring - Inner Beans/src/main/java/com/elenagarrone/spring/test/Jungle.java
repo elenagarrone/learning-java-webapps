7571877fb50f4b588669433d0ac27b5f45f5f937
elenagarrone <elena.15@hotmail.it>
2015-09-15 14:47:49 +0100

    spring tutorial 13-14

--- Spring - Inner Beans/src/main/java/com/elenagarrone/spring/test/Jungle.java
+++ Spring - Inner Beans/src/main/java/com/elenagarrone/spring/test/Jungle.java
@@ -1,41 +1,25 @@
 package com.elenagarrone.spring.test;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Jungle {
-	private Animal largest;
-	private List<Animal> animals;
+
+	private Map<String, String> foods = new HashMap<String, String>();
 	
-	public Animal getLargest() {
-		return largest;
-	}
-	
-	public void setLargest(Animal largest) {
-		this.largest = largest;
-	}
-	
-	public List<Animal> getAnimals() {
-		return animals;
-	}
-	
-	public void setAnimals(List<Animal> animals) {
-		this.animals = animals;
+	public void setFoods(Map<String, String> foods) {
+		this.foods = foods;
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		
-		sb.append("Largest: " + largest);
-		sb.append("\n");
-		sb.append("Others:\n");
-		
-		for(Animal animal: animals) {
-			sb.append(animal);
-			sb.append("\n");
+		for(Map.Entry<String, String> entry: foods.entrySet()) {
+			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
 		}
+		
 		return sb.toString();
 	}
-	
 
 }
