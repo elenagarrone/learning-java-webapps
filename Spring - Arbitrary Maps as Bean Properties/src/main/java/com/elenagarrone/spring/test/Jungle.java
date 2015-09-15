eca3c46f5e6938cdcf8db22324bee1533737fc7f
elenagarrone <elena.15@hotmail.it>
2015-09-15 15:53:23 +0100

    fixed mistakes

--- Spring - Arbitrary Maps as Bean Properties/src/main/java/com/elenagarrone/spring/test/Jungle.java
+++ Spring - Arbitrary Maps as Bean Properties/src/main/java/com/elenagarrone/spring/test/Jungle.java
@@ -1,14 +1,19 @@
 package com.elenagarrone.spring.test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Jungle {
+
 	private Map<String, String> foods = new HashMap<String, String>();
+	private Map<String, Animal> animals = new HashMap<String, Animal>();
 	
 	public void setFoods(Map<String, String> foods) {
 		this.foods = foods;
+	}
+	
+	public void setAnimals(Map<String, Animal> animals) {
+		this.animals = animals;
 	}
 
 	@Override
@@ -18,8 +23,14 @@ public class Jungle {
 		for(Map.Entry<String, String> entry: foods.entrySet()) {
 			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
 		}
+		
+		sb.append("\n");
+		
+		for(Map.Entry<String, Animal> entry: animals.entrySet()) {
+			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
+		}
+		
 		return sb.toString();
 	}
-	
 
 }
--- Spring - Inner Beans/src/main/java/com/elenagarrone/spring/test/Jungle.java
+++ Spring - Inner Beans/src/main/java/com/elenagarrone/spring/test/Jungle.java
@@ -1,25 +1,41 @@
 package com.elenagarrone.spring.test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class Jungle {
-
-	private Map<String, String> foods = new HashMap<String, String>();
+	private Animal largest;
+	private List<Animal> animals;
 	
-	public void setFoods(Map<String, String> foods) {
-		this.foods = foods;
+	public Animal getLargest() {
+		return largest;
+	}
+	
+	public void setLargest(Animal largest) {
+		this.largest = largest;
+	}
+	
+	public List<Animal> getAnimals() {
+		return animals;
+	}
+	
+	public void setAnimals(List<Animal> animals) {
+		this.animals = animals;
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		
-		for(Map.Entry<String, String> entry: foods.entrySet()) {
-			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
-		}
+		sb.append("Largest: " + largest);
+		sb.append("\n");
+		sb.append("Others:\n");
 		
+		for(Animal animal: animals) {
+			sb.append(animal);
+			sb.append("\n");
+		}
 		return sb.toString();
 	}
+	
 
 }
